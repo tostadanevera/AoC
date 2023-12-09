a1dcc4b2f8488f3c75d5ebf4fdd6e6df22492f61
tostada <mitostadoraquemada@gmail.com>
2023-12-09 21:44:50 +0100

    day 9 python

---
+++ 2023/day09/python/part1.py
@@ -0,0 +1,18 @@
+data = open('../in').read().split('\n')
+
+sum = 0
+
+def calc(nums):
+    if all(n == 0 for n in nums): return 0
+    else:
+        aux = []
+        for i in range(len(nums) - 1):
+            aux.append(nums[i + 1] - nums[i])
+        return calc(aux) + nums[len(nums) - 1]
+
+
+for line in data:
+    nums = list(map(int, line.split()))
+    sum += calc(nums)
+        
+print(sum)
---
+++ 2023/day09/python/part2.py
@@ -0,0 +1,18 @@
+data = open('../in').read().split('\n')
+
+sum = 0
+
+def calc(nums):
+    if all(n == 0 for n in nums): return 0
+    else:
+        aux = []
+        for i in range(1, len(nums)):
+            aux.append(nums[i] - nums[i - 1])
+        return nums[0] - calc(aux)
+
+
+for line in data:
+    nums = list(map(int, line.split()))
+    sum += calc(nums)
+        
+print(sum)
